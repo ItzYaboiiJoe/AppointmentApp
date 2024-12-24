c5a3b62e5b27a2e0ff6caa9702c33832b4a3538b
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-12-24 10:12:34 -0500

    Updated Styles

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 function Calendar() {
-  const [events, setEvents] = useState([
-    {
-      title: "Meeting",
-      start: "2024-12-01T10:00:00",
-      end: "2024-12-01T11:00:00",
-    },
-  ]);
-
-  const handleDateClick = (info) => {
-    const title = prompt("Enter Event Title:");
-    if (title) {
-      setEvents([...events, { title, start: info.dateStr, end: info.dateStr }]);
-    }
-  };
-
   return (
     <div className="px-4 py-8">
       <FullCalendar
@@ -30,8 +14,6 @@ function Calendar() {
           center: "title",
           right: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
-        events={events} // Attach events state
-        dateClick={handleDateClick} // Attach date click handler
         editable={true}
         selectable={true}
         height={765}
