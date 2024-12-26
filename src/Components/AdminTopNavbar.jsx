5f8108e18151f6f296dca0a0d670f2b21c5c694f
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-12-26 15:06:11 -0500

    Small Shadow Tweak

--- src/Components/AdminTopNavbar.jsx
+++ src/Components/AdminTopNavbar.jsx
@@ -2,7 +2,7 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 
 function AdminTopNavbar({ pageTitle }) {
   return (
-    <header className="w-full bg-Primary h-20 flex items-center justify-between px-6 shadow-md">
+    <header className="w-full bg-Primary h-20 flex items-center justify-between px-6 shadow-lg">
       <h1 className="text-black text-xl font-medium">{pageTitle}</h1>
       <button className="flex items-center bg-[#E9C46A] text-black px-4 py-2 rounded-lg shadow hover:bg-[#F4A261]">
         <FaRegCalendarAlt className="mr-2" />
