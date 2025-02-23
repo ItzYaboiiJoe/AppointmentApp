2c945fddacd940cd0d493f62ebed43b72e362e1b
ItzYaboiiJoe <josephakseoudy@gmail.com>
2025-02-23 00:48:07 -0500

    Fixed Services Cards bug if there were 2 or more services created

--- src/Pages/AdminServices.jsx
+++ src/Pages/AdminServices.jsx
@@ -1,12 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AdminSideNavbar from "../Components/AdminSideNavbar";
 import AdminTopNavbar from "../Components/AdminTopNavbar";
 import AdminServicesCards from "../Components/AdminServicesCards";
 import { IoAddCircleOutline } from "react-icons/io5";
 import AddNewCardModal from "../Components/AddNewCardModal";
+import { fireStore } from "../Config/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function AdminServices() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [services, setServices] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const servicesCollectionRef = collection(
+          fireStore,
+          "Joe BarberShop",
+          "Services",
+          "Services"
+        );
+        const servicesSnapshot = await getDocs(servicesCollectionRef);
+        const servicesList = servicesSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setServices(servicesList);
+      } catch (error) {
+        setError("Error fetching services: " + error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   function handleOpenModal() {
     return setIsModalOpen(true);
@@ -14,6 +44,16 @@ function AdminServices() {
 
   function handleCloseModal() {
     return setIsModalOpen(false);
+  }
+
+  function noServices() {
+    if (!loading && services.length === 0) {
+      return (
+        <div className="max-w-sm bg-red-100 rounded-lg shadow-xl p-4 mb-4">
+          <p className="text-gray-700 text-xl font-bold">No services Created</p>
+        </div>
+      );
+    }
   }
 
   return (
@@ -32,9 +72,27 @@ function AdminServices() {
           >
             <IoAddCircleOutline size={35} />
           </button>
-          {/* Services Cards */}
+          {/* Loading Message */}
+          {loading && (
+            <div className="max-w-sm bg-blue-100 rounded-lg shadow-xl p-4 mb-4">
+              <p className="text-gray-700 text-xl font-bold">
+                Loading services...
+              </p>
+            </div>
+          )}
+          {/* Error for when there is issues with fetching */}
+          {error && (
+            <div className="max-w-sm bg-red-100 rounded-lg shadow-xl p-4 mb-4">
+              <p className="text-red-500">{error}</p>
+            </div>
+          )}
+          {/* No Services message */}
           <div className="grid grid-cols-4 gap-4 m-6">
-            <AdminServicesCards />
+            {noServices()}
+            {/* Services Cards */}
+            {services.map((service) => (
+              <AdminServicesCards key={service.id} service={service} />
+            ))}
           </div>
         </div>
         {isModalOpen && <AddNewCardModal onClose={handleCloseModal} />}
