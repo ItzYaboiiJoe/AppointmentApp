a13f1683259a1c011684cc0991d47dc079f84df4
ItzYaboiiJoe <josephakseoudy@gmail.com>
2025-02-23 11:29:58 -0500

    Customized the calendar and added all year long view

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -2,21 +2,34 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import multiMonthPlugin from "@fullcalendar/multimonth";
 
 function Calendar() {
   return (
-    <div className="px-4 py-8">
+    <div className="calendar-container">
       <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          multiMonthPlugin,
+        ]}
         initialView="dayGridMonth"
         headerToolbar={{
           left: "prev,next today",
           center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay",
+          right: "multiMonthYear,dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        buttonText={{
+          today: "Today",
+          month: "Month",
+          week: "Week",
+          day: "Day",
+          multiMonthYear: "Year",
         }}
         editable={true}
         selectable={true}
-        height={765}
+        height="100%"
       />
     </div>
   );
