bcc608a747c112136caf322722da3ac3f4e36b07
ItzYaboiiJoe <josephakseoudy@gmail.com>
2025-03-05 20:41:42 -0500

    Adjusted The Settings UI

--- src/Components/BusinessInfoForm.jsx
+++ src/Components/BusinessInfoForm.jsx
@@ -1,28 +1,37 @@
 function BusinessInfoForm() {
   return (
     <form>
-      <div className="flex flex-col space-y-2">
+      <div className="flex flex-col space-y-2 ml-5">
+        <h2 className="text-Primary text-2xl my-2">Business Information</h2>
+        {/* Business Name */}
         <div>
-          <h2>Business Information</h2>
           <input
             type="text"
             placeholder="Name"
-            className="border-solid border border-black"
+            className="border-solid border border-black w-64"
           />
         </div>
+        {/* Business Address */}
         <div>
           <input
             type="text"
             placeholder="Street Address"
-            className="border-solid border border-black"
+            className="border-solid border border-black w-64"
           />
         </div>
+        {/* Business Type */}
         <div>
           <input
             type="text"
             placeholder="Business Type"
-            className="border-solid border border-black"
+            className="border-solid border border-black w-64"
           />
+        </div>
+        {/* Business Hours */}
+        <div>
+          <button className="bg-Primary text-white px-4 py-1 shadow-xl rounded hover:bg-[#1e6f65] w-64">
+            Business Hours
+          </button>
         </div>
       </div>
     </form>
--- src/Pages/AdminSettings.jsx
+++ src/Pages/AdminSettings.jsx
@@ -4,15 +4,17 @@ import BusinessInfoForm from "../Components/BusinessInfoForm";
 
 function AdminSettings() {
   return (
-    // side Navbar
-    <div className="flex h-screen">
-      <AdminSideNavbar />
-      {/* Top Navbar */}
-      <div className="flex-1 flex flex-col">
-        <AdminTopNavbar pageTitle="Settings" />
-        {/* Body Content */}
-        <div className="bg-[#EAF4F1] p-6">
-          <BusinessInfoForm />
+    <div className="bg-[#EAF4F1]">
+      {/* side Navbar */}
+      <div className="flex h-screen">
+        <AdminSideNavbar />
+        {/* Top Navbar */}
+        <div className="flex-1 flex flex-col">
+          <AdminTopNavbar pageTitle="Settings" />
+          {/* Body Content */}
+          <div className=" p-6">
+            <BusinessInfoForm />
+          </div>
         </div>
       </div>
     </div>
